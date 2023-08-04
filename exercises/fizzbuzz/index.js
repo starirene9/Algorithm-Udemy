3c5c5270522b7f26b8e4d9d64ecb148e61632a63
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-08-04 18:32:36 +0900

    fizzbuzz 다수 출제 된다고 함

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,39 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) { //15
+
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 === 0) {
+            console.log('fizzbuzz');
+        } else if (i % 3 === 0) {
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
 
 module.exports = fizzBuzz;
+
+// modulo operator
+// 9 % 3 === 0 // true
+// 10 % 3 === 0 // false
+
+
+// function fizzBuzz(n) {
+//     for(let i = 1 ; i <= n; i++){
+//         // 3과 5의 배수인지를 먼저 해줘야지 3의 fizz가 안나오고 이거 먼저 동작함
+//         if(i % 15 === 0) {
+//             console.log('fizzbuzz');
+//         } else if(i % 3 === 0) { // else if 를 사용해야 fizz가 한번 더 안 찍힘
+//             console.log('fizz');
+//         } else if(i % 5 === 0) {
+//             console.log('buzz');
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
