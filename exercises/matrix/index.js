5d0a27379c197ad233f37cead3d0997ca012e7f3
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-08-12 13:45:00 +0900

    Matirx.. so difficult...!

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -10,11 +10,63 @@
 //     [8, 9, 4],
 //     [7, 6, 5]]
 //  matrix(4)
-//     [[1,   2,  3, 4],
+//     [[1,  2,  3, 4],
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
+// spiraling in fashion
+// 종이에 적고 규칙을 찾아서 시작할 것 ~!
 
-function matrix(n) {}
+function matrix(n) {
+    const results = [];
+
+    for (let i = 0; i < n; i++) {
+        results.push([]); // [[],[]]
+    }
+
+    let counter = 1; // starting at 1
+    let startColumn = 0;
+    let endColumn = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+        // Top Row
+        for (let i = startColumn; i <= endColumn; i++) {
+            results[startRow][i] = counter;
+            counter++;
+        }
+        startRow++;
+
+        // Right Column
+        for (let i = startRow; i <= endRow; i++) {
+            results[i][endColumn] = counter;
+            counter++;
+        }
+        endColumn--;
+
+        // Bottom Row : Decrement
+        for (let i = endColumn; i >= startColumn; i--) {
+            results[endRow][i] = counter;
+            counter++;
+        }
+        endRow--;
+
+        // start column
+        for (let i = endRow; i >= startRow; i--) {
+            results[i][startColumn] = counter;
+            counter++;
+        }
+        startColumn++;
+    }
+
+    return results;
+}
 
 module.exports = matrix;
+
+// 1) const arr = [];
+// arr[3] = "Hi, there!" // [null, null, null, "Hi there!"]
+
+
+
