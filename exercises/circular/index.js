49914de925838afec0ab199a5709ae4c0afc56b3
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-08-23 21:18:11 +0900

    circular list solution

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -9,9 +9,41 @@
 //   l.head = a;
 //   a.next = b;
 //   b.next = c;
-//   c.next = b;
+//   c.next = b;  // 전의 것을 가리킴
 //   circular(l) // true
+// infinite nature
 
-function circular(list) {}
+
+function circular(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            return true;
+        }
+    }
+    return false;
+}
 
 module.exports = circular;
+
+// function circular(list) {
+//     //facing first element of the list
+//     let slow = list.getFirst(); // 1 loop
+//     let fast = list.getFirst(); // 2 loops
+//
+//     // while loop doesn't have en end to it
+//     while(fast.next && fast.next.next){
+//         // define it first
+//         slow = slow.next;
+//         fast = fast.next.next;
+//
+//         if(slow === fast){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
