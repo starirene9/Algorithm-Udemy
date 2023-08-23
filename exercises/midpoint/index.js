701c134ebc0cce7706b9cf7dda1d4f09ec2e9a97
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-08-23 21:05:12 +0900

    midpoint slow and fast variables

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -11,7 +11,38 @@
 //   l.insertLast('b')
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
+// middle ? half - l (less)
+// strategy
+// apply to even and odd number
+// 1. define center node : slow(first node), fast(second node) two variables
+// 2. start with head
+// 3. slow + 1, fast + 2 elements
+// 4. fast node no more ?  -> the end of the node
+// 5. slow 노드가 가리키는게 mid point 임
 
-function midpoint(list) {}
+
+function midpoint(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while(fast.next && fast.next.next) { // check next and next after that
+        slow = slow.next;
+        fast = fast.next.next; // jump for 2 step;
+    }
+
+    return slow;
+}
 
 module.exports = midpoint;
+// 1)
+// function midpoint(list) {
+//     let slow = list.getFirst(); // list.head() is also okay
+//     let fast = list.getFirst();
+//
+//     while(fast.next && fast.next.next) { // check next and next after that
+//         slow = slow.next;
+//         fast = fast.next.next; // jump for 2 step;
+//     }
+//
+//     return slow;
+// }
