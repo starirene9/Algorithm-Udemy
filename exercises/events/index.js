144d926ace699684f5568d387c767f65048e0821
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-08-27 23:33:29 +0900

    events

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -2,18 +2,38 @@
 // Create an 'eventing' library out of the
 // Events class.  The Events class should
 // have methods 'on', 'trigger', and 'off'.
+// basic javascript concept of the library
 
 class Events {
-  // Register an event handler
-  on(eventName, callback) {}
+    // Register an event handler
 
-  // Trigger all callbacks associated
-  // with a given eventName
-  trigger(eventName) {}
+    constructor() {
+        this.events = {}; // an empty object
+    }
 
-  // Remove all event handlers associated
-  // with the given eventName
-  off(eventName) {}
+    on(eventName, callback) {
+        if (this.events[eventName]) { // [eventName] added as a key
+            this.events[eventName].push(callback); // 이미 eventName 이 있는 경우라면 simply push callback
+        } else {
+            this.events[eventName] = [callback];
+        }
+    }
+
+    // Trigger all callbacks associated
+    // with a given eventName
+    trigger(eventName) {
+        if (this.events[eventName]) { // exist
+            for (let cb of this.events[eventName]) { // iterate through it
+                cb(); //cb = callback
+            }
+        }
+    }
+
+    // Remove all event handlers associated
+    // with the given eventName
+    off(eventName) {
+        delete this.events[eventName];
+    }
 }
 
 module.exports = Events;
