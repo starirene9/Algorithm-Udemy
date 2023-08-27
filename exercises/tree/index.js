986bbcbd163dd4279ffee332fa994d3dc16982c4
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-08-28 00:06:49 +0900

    tree

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,32 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.children = [];
+    }
 
-class Tree {}
+    add(data) {
+        this.children.push(new Node(data));
+    }
 
-module.exports = { Tree, Node };
+    remove(data) {
+        this.children = this.children.filter(node =>{
+            return node.data !== data; // does not equal
+        })
+    }
+}
+
+class Tree {
+
+}
+
+module.exports = {Tree, Node};
+
+// let arr = [1,2,3];
+//
+// arr = arr.filter(element =>{
+//    return element === 1;
+// });
+// filter 함수는 create new array
