4220c256099e3c7f1112d29e2dbddeced736a232
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-08-30 00:03:33 +0900

    levelwidth algorithm

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,28 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const arr = [root, 's'];
+    const counter = [0];
+
+    while(arr.length > 1) {
+        const node = arr.shift();
+
+        if(node === 's') {
+            counter.push(0);
+            arr.push('s');
+        } else {
+            arr.push(...node.children);
+            counter[counter.length - 1]++; // the current level of our tree
+        }
+    }
+
+    return counter;
+}
+
+//Counters = 0
+
+//Array
+// a root node , token : 토큰 하나 남았을때는 stop ,
 
 module.exports = levelWidth;
