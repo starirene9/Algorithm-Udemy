319860d8f222b07fb2953c3cbcd5b193bf00c5ac
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-09-01 01:08:18 +0900

    selectionSort 선택정렬

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -14,6 +14,8 @@ function bubbleSort(arr) {
             }
         }
     }
+    return arr;
+//     리턴하는 것을 잊지 말거라~!
 }
 // less efficient algorithm
 // 옆의 숫자와 비교해서 큰 수를 뒤로 빼고
@@ -22,8 +24,27 @@ function bubbleSort(arr) {
 // 대부분의 실제 애플리케이션에서는 다른 정렬 알고리즘을 사용하는 것이 좋습니다.
 
 function selectionSort(arr) {
+    for (let i = 0; i <arr.length; i++) {
+        let indexOfMin = i; // prove me wrong
 
+        for (let j = i + 1; j <arr.length ; j++){
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+        if (indexOfMin !== i) {
+            let lesser = arr[indexOfMin];
+            arr[indexOfMin] = arr[i];
+            arr[i] = lesser;
+        }
+    }
+    return arr;
 }
+// Prove me Wrong Algorithm
+// we assume i to indexOfMin : 가장 적은 숫자라고 가정
+// show me with other numbers are smaller than this!
+// less than the current element ? update indexofMin to the value less
+// 작은 수를 indexofMin 에 대입해나가는 과정이다. 그리고 swap 하기
 
 function mergeSort(arr) {
 
